7d7159ac9312bcd892265cae2fa2060de3e36883
priyankathodeti <priyankathodeti1@gmail.com>
2024-02-24 21:02:29 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
  dotenv.config();
 mongoose.connect(
     process.env.Mongo
@@ -14,9 +14,11 @@ mongoose.connect(
     })
 const app = express();
 
+ app.use(express.json());
 app.listen(3000, () => (
     console.log("server is running")
 ));
 
 
 app.use('/app/user',userRoutes);
+app.use('/app/auth',authRoutes);
